abdb67097cf03df4179588fc003619a50733e612
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-12-03 21:06:06 +0100

    lade till några consol.loggar

--- js/services/cityApi.js
+++ js/services/cityApi.js
@@ -17,10 +17,11 @@ export async function cityApi(query) {
         
         // Om inga resultat → returera tom array
         if (!data.results) return [];
+        console.log("Här fanns det ingen data..");
 
         // Returnera ALLA råa träffar 
         return data.results;
-        console.log(data.results)
+        console.log(data.results);
 
     } catch (error) {
         console.error("cityApi error:", error);
