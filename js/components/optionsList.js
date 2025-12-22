bbb30b1001344f0752795d03a70c42606d8cb07c
FiorellaDiGiulio <fiorella.digiulio@chasacademy.se>
2025-12-22 11:52:40 +0100

    css

--- js/components/optionsList.js
+++ js/components/optionsList.js
@@ -37,7 +37,7 @@ export function renderOptions(matches, onSelect) {
         const btn = document.createElement("button");
         btn.textContent = `${match.name}, ${match.country}`;
         btn.className = "city-option";
-        btn.setAttribute("tabindex", "-1");
+        btn.setAttribute("tabindex", "0");
 
         btn.addEventListener("mouseenter", () => {
             hoverIndex = index;
